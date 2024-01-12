d1be715642fc6dd7661213a43890c074c35f6a3d
zakariaAmeen2020 <136131826+zakariaAmeen2020@users.noreply.github.com>
2024-01-12 21:47:45 +0300

    Update baseURL.js

--- src/Api/baseURL.js
+++ src/Api/baseURL.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 
-const baseUrl = axios.create({ baseURL: "http://127.0.0.1:1000" })
+const baseUrl = axios.create({ baseURL:BASE_URL=https://sdgsdg.onrender.com
+})
 
 export default baseUrl
