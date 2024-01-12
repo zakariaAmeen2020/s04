0ee83fa7fde8df2acc5f5fe0fca3ee86c9e9bb32
zakariaAmeen2020 <136131826+zakariaAmeen2020@users.noreply.github.com>
2024-01-13 00:02:17 +0300

    Update baseURL.js

--- src/Api/baseURL.js
+++ src/Api/baseURL.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const baseUrl = axios.create({ baseURL:"http://127.0.0.1:10000"
+const baseUrl = axios.create({ baseURL:"http://127.0.0.1:1000"
 })
 
 export default baseUrl
